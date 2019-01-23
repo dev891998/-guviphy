7845a0f197cf9cbbe1b96f60b197eb21c442a37c
dev891998 <43547808+dev891998@users.noreply.github.com>
2019-01-23 18:32:03 +0530

    Create Largest among 3 numbers.py

---
+++ Largest among 3 numbers.py
@@ -0,0 +1,9 @@
+a=float(input())
+b=float(input())
+c=float(input())
+if(a>b) and (a>c):
+	print("a is the largest")
+elif(b>a) and (b>c):
+	print("b is the largest")
+else:
+	print("c is the largest")
